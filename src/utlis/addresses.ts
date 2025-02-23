5dec8ded9d02d28c0bc2e6da635d8af7c5a58728
DengreSarthak <dengresathak2002@gmail.com>
2025-02-23 21:15:46 +0530

    Contract changes

--- src/utlis/addresses.ts
+++ src/utlis/addresses.ts
@@ -1,5 +1,5 @@
 export const PlazaFactoryAddress = {
-  534351: "0xbcdf92918137813a61fdc1b6fc511e4c87b5609d",
+  534351: "0xd7d062cd1af0b981414b03560a39827318f77438",
 } as {
   [key: number]: `0x${string}`;
 };
