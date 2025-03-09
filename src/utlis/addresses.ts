b317b242348bbe75aba4a85458d3eefd5e2cdfac
DengreSarthak <dengresathak2002@gmail.com>
2025-03-09 09:33:17 +0530

    Deployed on Ethereum Classic Network

--- src/utlis/addresses.ts
+++ src/utlis/addresses.ts
@@ -1,5 +1,6 @@
 export const PlazaFactoryAddress = {
-  534351: "0xdf441aeb73ec4829a75f1ea666c9aa1f4da6fc8f",
+  // 534351: "0xdf441aeb73ec4829a75f1ea666c9aa1f4da6fc8f",
+  61: "0x553859A7E3f8841aed994a2a131eBFB38Aa9dc10",
 } as {
   [key: number]: `0x${string}`;
 };
--- src/utlis/config.ts
+++ src/utlis/config.ts
@@ -1,11 +1,11 @@
 import { scrollSepolia } from "wagmi/chains";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-// import { ethereumClassic } from '@/components/EthereumClassic'
+import { ethereumClassic } from "@/components/EthereumClassic";
 // import { milkomeda } from '@/components/Milkomeda'
 
 export const config = getDefaultConfig({
   appName: "Plaza",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? "",
-  chains: [scrollSepolia],
+  chains: [ethereumClassic],
   ssr: true,
 });
