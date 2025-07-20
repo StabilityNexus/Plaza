8cdabbd64855240716d1b671fe01b6b69eb09dde
Sarthak Dengre <134192334+DengreSarthak@users.noreply.github.com>
2025-07-20 20:27:36 +0530

    Next config changes

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,9 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Plaza';
+
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
   images: {
       unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
